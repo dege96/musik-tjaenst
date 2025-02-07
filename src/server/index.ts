9423fc6db01458256d07a2554427e5b809833096
dege96 <victordegeer96@gmail.com>
2025-02-07 01:04:10 +0100

    Update API configuration for production

--- src/server/index.ts
+++ src/server/index.ts
@@ -28,7 +28,9 @@ const corsOptions = {
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    exposedHeaders: ['Content-Range', 'X-Content-Range'],
+    maxAge: 600,
     optionsSuccessStatus: 200
 };
 
